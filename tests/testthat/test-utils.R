eca14388e4f0b2d0590b8a3675d59913a2ff172b
Daniel Antal <antaldaniel@gmail.com>
2025-07-16 20:08:11 +0200

    update tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -5,12 +5,9 @@ test_that("get_data() works", {
 })
 
 test_reset_file <- file.path(tempdir(), "test_reset.ttl")
-writeLines(text="reset", test_reset_file)
+writeLines(text = "reset", test_reset_file)
 reset_text_file(test_reset_file)
 
 test_that("get_data() works", {
   expect_equal(readLines(test_reset_file), "")
 })
-
-
-  
